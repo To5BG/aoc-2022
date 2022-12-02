9f69fc4113ffd29950b3e4f548e519a9a4e158ee
AlpiGun5 <alperen.tf2@gmail.com>
2022-12-02 19:24:59 +0100

    Day 2, refactor

--- src/solutions/Day2.java
+++ src/solutions/Day2.java
@@ -4,14 +4,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.function.BiFunction;
 
 @SuppressWarnings("unused")
 public class Day2 {
-    public static Map<String, Integer> lookupStar1 = new HashMap<>();
-    public static Map<String, Integer> lookupStar2 = new HashMap<>();
-
     public static Object preprocess(Path filepath, int star) {
         String input;
         try {
@@ -22,24 +18,12 @@ public class Day2 {
         }
         String[] rounds = input.split("\n");
         star = Math.min(Math.max(star, 1), 2);
-        for (int i = 0; i < 3; i++) {
-            // x y z
-            lookupStar1.put((char)(88 + i) + "", i + 1);
-            lookupStar2.put((char)(88 + i) + "", 3 * i);
-            // a z, b y, c x
-            lookupStar1.put((char)(65 + i) + " " + (char)(90 - i), 3 * i);
-            lookupStar2.put((char)(65 + i) + " " + (char)(90 - i), 2);
-            // b x, c z, a y
-            lookupStar1.put((char)(65 + (i + 1) % 3) + " " + (char)(90 - (i + 2) % 3), 3 * i);
-            lookupStar2.put((char)(65 + (i + 1) % 3) + " " + (char)(90 - (i + 2) % 3), 1);
-            // c y, a x, b z
-            lookupStar1.put((char)(65 + (i + 2) % 3) + " " + (char)(90 - (i + 1) % 3), 3 * i);
-            lookupStar2.put((char)(65 + (i + 2) % 3) + " " + (char)(90 - (i + 1) % 3), 3);
-        }
-        return solve(rounds, star == 1 ? lookupStar1 : lookupStar2);
+        return solve(rounds, star == 1
+                ? (a, b) -> (1 + b) + 3 * ((b - a + 4) % 3)
+                : (a, b) -> (3 * b) + (1 + ((a + b + 2) % 3)));
     }
-    public static Object solve(String[] input, Map<String, Integer> lookup) {
-        return Arrays.stream(input).map(s -> lookup.get(s) + lookup.get(s.charAt(2) + ""))
+    public static Object solve(String[] input, BiFunction<Integer, Integer, Integer> f) {
+        return Arrays.stream(input).map(s -> f.apply(s.charAt(0) - 65, s.charAt(2) - 88))
                 .reduce(0, Integer::sum);
     }
 }
